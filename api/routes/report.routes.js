54e55d87284ade192488acf7a1ee875d43b25390
Omar_Ahmed20 <109938759+Omar-Einstein7@users.noreply.github.com>
2025-12-06 21:50:17 +0200

    ff

--- api/routes/report.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const upload = require("../middleware/upload");
-const ctrl = require("../controllers/report.controller");
-
-router.post("/", upload.single("image"), ctrl.createReport);
-router.get("/", ctrl.getReports);
-
-module.exports = router;
